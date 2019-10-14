ac515d349b0db2665dccdf83cd16bc3b3e7e64d3
SchoolAccB <bbrands@roc-dev.com>
2019-10-14 12:01:32 +0200

    Opdracht 5

--- index.php
+++ index.php
@@ -15,14 +15,13 @@
 <img id="logobar" src="Resources/monkey_swings.png">
 
 <?php
+$db = mysqli_connect('localhost', 'root', 'toor', 'apen', '8889');
 try {
-$dbh = new PDO('mysql:host=localhost;port=8889;dbname=apen', "root", "toor");
+    $dbh = new PDO('mysql:host=localhost;port=8889;dbname=apen', "root", "toor");
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    }
-catch(PDOException $e)
-    {
+} catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
-    }
+}
 
 $stmtapen = $dbh->prepare("SELECT * FROM aap");
 $stmtapen->execute();
@@ -30,23 +29,48 @@ $apen = $stmtapen->fetchAll();
 
 echo "<ul>";
 
-foreach($apen as $aap) { ?>
-<li><a href="https://www.google.nl/search?q=<?=$aap['soort']?>&tbm=isch"><?=$aap['soort']?></a></li>
+foreach ($apen as $aap) { ?>
+    <li><a href="https://www.google.nl/search?q=<?= $aap['soort'] ?>&tbm=isch"><?= $aap['soort'] ?></a></li>
 <?php }
 
-echo "</ul>";
+echo "</ul>"; ?>
+
+<div id="leefgebied">
+    <form id="leefgebiedinput" action="" method="post">
+        <input type="text" name="idleefgebied" placeholder="ID">
+        <input type="text" name="omschrijvingleefgebied" placeholder="Leefgebied">
+        <button type="submit" name="submitleefgebied">Submit</button>
+    </form>
+</div>
+
+<?php
+$leefgebiedId = 0;
+$leefgebiedOmschrijving = "";
+
+if (isset($_POST['submitleefgebied'])) {
+    $leefgebiedId = $_POST['idleefgebied'];
+    $leefgebiedOmschrijving = $_POST['omschrijvingleefgebied'];
+
+    $sql = "INSERT INTO leefgebied (idleefgebied, omschrijving) VALUES ('$leefgebiedId', '$leefgebiedOmschrijving')";
+    $dbh->exec($sql);
+}
 
 $stmtleefgebieden = $dbh->prepare("SELECT * FROM leefgebied");
 $stmtleefgebieden->execute();
-$leefgebieden = $stmtleefgebieden->fetchAll();
+$leefgebieden = $stmtleefgebieden->fetchAll(); ?>
 
-echo "<ul>";
+<table>
+    <tr>
+        <th>ID</th>
+        <th>Omschrijving</th>
+    </tr>
 
-foreach($leefgebieden as $leefgebied) {
-    echo "<li>".$leefgebied['omschrijving']."</li>";
-}
+    <?php foreach ($leefgebieden as $leefgebied) {
+        echo "<tr><td>". $leefgebied['idleefgebied'] ."</td><td>". $leefgebied['omschrijving'] . "</td></tr>";
+    }
 
-echo "</ul>";?>
+    echo "</ul>";
+    echo "</table>"; ?>
 
 </body>
 </html>
