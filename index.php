947c2669f5250a20840dc64940dba1694f195e3b
SchoolAccB <bbrands@roc-dev.com>
2019-10-14 09:22:50 +0200

    Opdracht 2

--- index.php
+++ index.php
@@ -1,7 +1,29 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: barry
- * Date: 14-10-2019
- * Time: 08:48
- */
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Monkey Business</title>
+    <link rel="stylesheet" href="index.css">
+    <link href="https://fonts.googleapis.com/css?family=Bangers&display=swap" rel="stylesheet">
+</head>
+<body>
+
+<img id="logo" src="Resources/monkey-business.jpg">
+
+<h1 id="logotext">Select your monkey!</h1>
+
+<img id="logobar" src="Resources/monkey_swings.png">
+
+<ul>
+    <li><a href="https://www.google.nl/search?q=Baviaan&tbm=isch">Baviaan</a></li>
+    <li><a href="https://www.google.nl/search?q=Guereza&tbm=isch">Guereza</a></li>
+    <li><a href="https://www.google.nl/search?q=Langoer&tbm=isch">Langoer</a></li>
+    <li><a href="https://www.google.nl/search?q=Neusaap&tbm=isch">Neusaap</a></li>
+    <li><a href="https://www.google.nl/search?q=Tamarin&tbm=isch">Tamarin</a></li>
+    <li><a href="https://www.google.nl/search?q=Brulaap&tbm=isch">Brulaap</a></li>
+    <li><a href="https://www.google.nl/search?q=Halfaap&tbm=isch">Halfaap</a></li>
+    <li><a href="https://www.google.nl/search?q=Mandril&tbm=isch">Mandril</a></li>
+</ul>
+
+</body>
+</html>
